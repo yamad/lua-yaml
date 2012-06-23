a88395bf7ffb3f669379c2cbac75a267aefae00c
Craig Barnes <cr@igbarn.es>
2012-06-23 10:51:34 +0100

    Add basic tests for yaml.configure and yaml.null

--- test.lua
+++ test.lua
@@ -1,20 +1,30 @@
 require "yaml"
 
+yaml.configure {
+    dump_auto_array = true,
+    dump_check_metatables = true,
+    dump_error_on_unsupported = true,
+    load_set_metatables = true,
+    load_numeric_scalars = true,
+    load_nulls_as_nil = false
+}
+
 local doc = yaml.load[[
 %YAML 1.1
 ---
-map: Wrapped
-     line
-
+empty: ~
+map:   Wrapped
+       line
 map2:
-  seq:
-  - libyaml     # C Fast YAML 1.1
-  - lua-yaml    # Lua binding to libyaml
+    seq:
+    - libyaml     # C Fast YAML 1.1
+    - lua-yaml    # Lua binding to libyaml
 ...
 ]]
 
 assert(doc.map == "Wrapped line",       'map value should be "Wrapped line"')
 assert(doc.map2.seq[1] == "libyaml",    'seq[1] value should be "libyaml"')
 assert(#doc.map2.seq == 2,              'length of seq should be 2')
+assert(doc.empty == yaml.null,          'empty value should equal yaml.null')
 
 print "All tests passed"
